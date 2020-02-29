478fcb67440c803ace1c83a284e0fc08c5670189
Rohan <rohanya.76426@gmail.com>
2020-02-29 22:44:44 +0530

    added checkPosition function and code for player 2

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -21,17 +21,18 @@ def boardStructure():
     """)
 
 
+def checkPosition(position):
+    if theBoard[position] == 'X' or theBoard[position] == 'O':
+        return True
+    else:
+        return False
+
+
 def checkWin():
     pass
 
 
-def intiBoard():
-    theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
-                'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
-                'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
-
-
-boardStructure()
+boardStructure()    # board structure for instructions
 player1 = input(' player1 select from "X" and "O" to play:')
 player2 = input(' player2 select from "X" and "O" to play:')
 
@@ -42,17 +43,34 @@ indexes = {1: 'top-L', 2: 'top-M', 3: 'top-R',
            4: 'mid-L', 5: 'mid-M', 6: 'mid-R',
            7: 'low-L', 8: 'low-M', 9: 'low-R'}
 
-turn1 = 1
+turn1 = 1   #can be replaced
 turn2 = 0
 
-for i in range(9):
+for i in range(1, 9):
     if turn1:
         while True:
             print(" Player 1 your turn, choose a position")
             pos = int(input('> '))
             if pos < 9 or pos > 1:
-                theBoard[indexes[pos]] = player1
-                turn1 = 0
-                turn2 = 1
-                printBoard()
-                break
+                if not checkPosition(indexes[pos]):
+                    theBoard[indexes[pos]] = player1
+                    turn1 = 0
+                    turn2 = 1
+                    printBoard()
+                    break
+                else:
+                    break
+    if turn2:
+        while True:
+            print(" Player 2 your turn, choose position: ")
+            pos = int(input('> '))
+            if pos < 9 or pos > 1:
+                if not checkPosition(indexes[pos]):
+                    theBoard[indexes[pos]] = player2
+                    turn2 = 0
+                    turn1 = 1
+                    printBoard()
+                    break
+                else:
+                    break
+
