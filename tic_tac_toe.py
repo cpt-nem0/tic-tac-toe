dc8db62d804237653ddb25648aa5bb7ba83a05db
Rohan <rohanya.76426@gmail.com>
2020-02-29 23:14:44 +0530

    optamized the code

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -10,14 +10,14 @@ def printBoard():
     )
 
 
-def boardStructure():
+def boardStructure():       #reason for this structure is the convenience on the keypad
     print("""
     Board Structure:
-        (1)|(2)|(3)
+        (7)|(8)|(9)
         ---+---+---
         (4)|(5)|(6)
         ---+---+---
-        (7)|(8)|(9)
+        (1)|(2)|(3)
     """)
 
 
@@ -33,44 +33,24 @@ def checkWin():
 
 
 boardStructure()    # board structure for instructions
-player1 = input(' player1 select from "X" and "O" to play:')
-player2 = input(' player2 select from "X" and "O" to play:')
+player1 = input(' player1 select from "X" and "O" to play:').upper()
+player2 = input(' player2 select from "X" and "O" to play:').upper()
 
 theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
             'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
             'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
-indexes = {1: 'top-L', 2: 'top-M', 3: 'top-R',
+indexes = {7: 'top-L', 8: 'top-M', 9: 'top-R',
            4: 'mid-L', 5: 'mid-M', 6: 'mid-R',
-           7: 'low-L', 8: 'low-M', 9: 'low-R'}
+           1: 'low-L', 2: 'low-M', 3: 'low-R'}
 
-turn1 = 1   #can be replaced
-turn2 = 0
-
+turn = player1
 for i in range(1, 9):
-    if turn1:
-        while True:
-            print(" Player 1 your turn, choose a position")
-            pos = int(input('> '))
-            if pos < 9 or pos > 1:
-                if not checkPosition(indexes[pos]):
-                    theBoard[indexes[pos]] = player1
-                    turn1 = 0
-                    turn2 = 1
-                    printBoard()
-                    break
-                else:
-                    break
-    if turn2:
-        while True:
-            print(" Player 2 your turn, choose position: ")
-            pos = int(input('> '))
-            if pos < 9 or pos > 1:
-                if not checkPosition(indexes[pos]):
-                    theBoard[indexes[pos]] = player2
-                    turn2 = 0
-                    turn1 = 1
-                    printBoard()
-                    break
-                else:
-                    break
-
+    while True:
+        print(f'Turn of the player who has chosen "{turn}", enter your desired position: ')
+        pos = int(input('> '))
+        if pos > 1 or pos < 9:
+            if not checkPosition(indexes[pos]):
+                theBoard[indexes[pos]] = player1
+                turn = player2
+                printBoard()
+                break
